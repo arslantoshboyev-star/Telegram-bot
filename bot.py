79d9b007ad90590f13b8b1fec7c310c6db36bdc1
arslantoshboyev-star <arslantoshboyev@gmail.com>
2026-07-18 22:30:22 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,13 @@
-8753208681:AAGxkCuTHvPKkzxtbNULQxp2zbQ0tWvUFC0
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+
+TOKEN = 
+8753208681:AAGxkCuTHvPKkzxtbNULQxp2zbQ0tWvUFCo
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("Salom!")
+
+app = ApplicationBuilder().token(TOKEN).build()
+app.add_handler(CommandHandler("start", start))
+
+app.run_polling()
